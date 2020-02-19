aa9b152d550e97615a70a6193a232347aa578bb1
Katakam Mallikarjun <mallik813@gmail.com>
2020-02-19 22:24:31 +0530

    linting

--- src/app.js
+++ src/app.js
@@ -1,22 +1,20 @@
-const express = require("express");
-const cors = require("cors");
-const config = require("./utils/config");
+const express = require('express');
+const cors = require('cors');
+const config = require('./utils/config');
 
 const app = express();
 app.use(cors());
 
-app.use("/api", (req, res) => {
-  res.send("It's working")
+app.use('/api', (req, res) => {
+  res.send("It's working");
 });
 
 // app.use("/users", require("./routes/users.js"));
 
-app.get("*", (req, res) => {
-  res.status(404).send("You did something wrong!");
+app.get('*', (req, res) => {
+  res.status(404).send('You did something wrong!');
 });
 
 const port = config.port || 3001;
 
-app.listen(port, () =>
-  console.log(`API server ready at http://localhost:${port}`);
-);
+app.listen(port, () => console.log(`API server ready at http://localhost:${port}`));
