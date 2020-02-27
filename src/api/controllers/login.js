e4ee7ce759c053640bbac6aef96e128a86134b9d
Katakam Mallikarjun <mallik813@gmail.com>
2020-02-27 13:58:46 +0530

    add spacing

--- src/api/controllers/login.js
+++ src/api/controllers/login.js
@@ -8,15 +8,18 @@ const handleLogin = async (req, res, db) => {
     res.status(400).send('Fields cannot be empty');
   } else {
     const exists = await db.collection('users').findOne({ email });
+
     if (!exists) res.status(401).send('Email does not exist');
     else {
       const match = await bcrypt.compare(password, exists.hash);
+
       if (match) {
         const payload = { email: exists.email, name: exists.name };
         const token = jwt.sign(payload, jwtSecret.privKey, {
           algorithm: 'ES512',
           expiresIn: '30d',
         });
+
         res.status(200).send(token);
       } else {
         res.status(400).send('Incorrect password');
